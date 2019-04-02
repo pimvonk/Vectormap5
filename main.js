c8e609d9c9c9a8f1fb2348ffd89c386353d1d8d7
pimvonk <34235291+pimvonk@users.noreply.github.com>
2019-04-02 12:42:55 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicGltdm9uayIsImEiOiJjanNsdm1ibmYwMmpxM3pwYm8ydGZkd2FrIn0.86tE7iRZ0eZVyrRfNPGExw';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/pimvonk/cjtsfqn4v003d1fpe3bptcb54',
+    hash: true,
+    zoom: 12,
+    pitch: 32,
+    bearing: -14.40,
+    center: [ 4.966521, 52.150485]
+});
